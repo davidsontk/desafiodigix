60cddf9d9ebf83cf1d3e6de41c7bb709bd1a7783
Davidson Ferreira <davidson.tk@hotmail.com>
2020-04-27 18:11:56 -0400

    revomendo comentario na main

--- src/main/java/com/digix/desafio/StarterApplication.java
+++ src/main/java/com/digix/desafio/StarterApplication.java
@@ -7,11 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StarterApplication {
 
     public static void main(String[] args) {
-        
-        SpringApplication.run(StarterApplication.class, args);
-        
-        //SpringApplication.run(StarterApplication.class, args);
-    }
 
+        SpringApplication.run(StarterApplication.class, args);
+
+    }
 
 }
